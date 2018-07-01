4a5fe5960ec6adfedc199c490cd52edf6fb6b6ef
Wellerson <wellerson010@hotmail.com>
2018-07-01 13:12:27 -0300

    Estruturando layout
    Integrando NavigationDrawer com Router
    Habilitando/desabilitando NavigationDrawer com menu hamburguer

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,18 @@
 <template>
     <v-app dark>
-        <v-navigation-drawer app></v-navigation-drawer>
-        <v-toolbar app></v-toolbar>
+        <v-navigation-drawer v-model="drawer" app>
+            <v-list>
+                <v-list-tile v-for="item in itemsDrawer" :key="item.title" :to="item.url">
+                    <v-list-tile-content>
+                         <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar app>
+            <v-toolbar-side-icon @click="drawer = !drawer"/>
+            <v-toolbar-title>Yu-Gi-Oh! Manager</v-toolbar-title>
+        </v-toolbar>
         <v-content>
             oi
         </v-content>
@@ -10,10 +21,30 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import NavigationDrawerItem from '@/models/NavigationDrawerItem';
 
 @Component
 export default class Main extends Vue {
+    drawer = false;
 
+    itemsDrawer: NavigationDrawerItem[] = [
+        {
+            title: 'Cards',
+            url: '/cards'
+        },
+        {
+            title: 'Deck',
+            url: '/deck'
+        },
+        {
+            title: 'Booster',
+            url: '/booster'
+        },
+        {
+            title: 'Store',
+            url: '/store'
+        }
+    ];
 }
 </script>
 
---
+++ src/models/NavigationDrawerItem.ts
@@ -0,0 +1,4 @@
+export default interface NavigationDrawerItem {
+    title: string;
+    url: string;
+}
