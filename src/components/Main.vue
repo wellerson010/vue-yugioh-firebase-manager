6fa77fb8a13aec1d5920a89d42af7f7c8f163cbd
Wellerson <wellerson010@hotmail.com>
2018-07-01 10:01:33 -0300

    Primeiro commit

---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app dark>
+        <v-navigation-drawer app></v-navigation-drawer>
+        <v-toolbar app></v-toolbar>
+        <v-content>
+            oi
+        </v-content>
+    </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Main extends Vue {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
+import App from '@/components/App.vue';
+import router from '@/router';
+import store from '@/vuex/store';
+import '@/registerServiceWorker';
+
+Vue.use(Vuetify);
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
